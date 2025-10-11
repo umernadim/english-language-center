97b73ba3d08c141d1130ff92fdf1b7ba6882b7eb
umernadim <itsumernadeem@gmail.com>
2025-10-11 13:09:00 +0500

    footer added

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,26 +23,25 @@ function navbarHandler() {
 
 navbarHandler();
 
-// code for carousel 
+// code for carousel
 function carouselHandler() {
   const images = document.querySelectorAll(".background-img");
   let current = 0;
 
   setInterval(() => {
-      images[current].style.opacity = 0;
-      current = (current + 1) % images.length;
-      images[current].style.opacity = 1;
-    }, 5000);
+    images[current].style.opacity = 0;
+    current = (current + 1) % images.length;
+    images[current].style.opacity = 1;
+  }, 5000);
 }
 
-carouselHandler()
+carouselHandler();
 
-// function to handle FAQ section 
+// function to handle FAQ section
 function faqHandler() {
-  
   const faqItems = document.querySelectorAll(".faq-item");
-  
-  faqItems.forEach(item => {
+
+  faqItems.forEach((item) => {
     const question = item.querySelector(".faq-question");
     question.addEventListener("click", () => {
       item.classList.toggle("active");
@@ -50,3 +49,16 @@ function faqHandler() {
   });
 }
 faqHandler();
+
+// Simple form submission handler for newsletter
+document
+  .querySelector(".newsletter-btn")
+  .addEventListener("click", function () {
+    const emailInput = document.querySelector(".newsletter-input input");
+    if (emailInput.value) {
+      alert("Thank you for subscribing to our newsletter!");
+      emailInput.value = "";
+    } else {
+      alert("Please enter your email address.");
+    }
+  });
